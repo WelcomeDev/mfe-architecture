cd31a21a49bfa97683596c943f9b06daa1ce9853
Fedor Ishcheko <fedor.ishchenko.18@gmail.com>
2025-06-04 13:50:39 +0300

    fix: bff /license/refresh method

--- app/bff/src/index.js
+++ app/bff/src/index.js
@@ -28,7 +28,7 @@ app.get('/license/has-option', (req, res) => {
 
 app.get('/license/refresh', (req, res) => {
     license.dueDate = new Date().toISOString();
-    res.status(200);
+    res.status(200).send();
 })
 
 app.post('/license/check-validity', async (req, res) => {
