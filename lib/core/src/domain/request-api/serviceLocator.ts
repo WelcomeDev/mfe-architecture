46617f843e913cc3b60d6ff5899fa9f0d38d408e
Fedor Ishcheko <fedor.ishchenko.18@gmail.com>
2025-06-04 14:00:19 +0300

    fix: refactor serviceLocator

--- lib/core/src/domain/request-api/serviceLocator.ts
+++ lib/core/src/domain/request-api/serviceLocator.ts
@@ -3,10 +3,8 @@ import { AsyncBus } from './asyncBus';
 
 const busInstance: Map<string, AsyncBus<any>> = new Map<string, AsyncBus<any>>();
 
-export const bus = ()=> busInstance;
-
 export function getBus<TApiClient extends IEvoApi>(apiName: string): AsyncBus<TApiClient> {
-    const busRegistry: Map<string, AsyncBus<any>> = bus();
+    const busRegistry: Map<string, AsyncBus<any>> = busInstance;
     if (!busRegistry.has(apiName)) {
         busRegistry.set(apiName, new AsyncBus<TApiClient>(apiName));
     }
